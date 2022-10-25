e01944e6536fb42a7ba9ee3d5cd097a6597f9e01
SwimmingPotato <ptphong200i@gmail.com>
2022-10-25 22:24:12 +0700

    Create basic app structure

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Case_MD3
+
 Case Module 3 Codegym
+
+# How to run
+
+git clone / git pull
+npm install
+npm run devStart
---
+++ src/controllers/authController.js
@@ -0,0 +1,8 @@
+module.exports.authController = (req, res) => {
+  res.end("Auth Controller");
+  // TODO: xử lí /signin, /signup
+  // signup check username unique, mã hoá password bằng argon2
+
+  // signin check thông tin đăng nhập và gửi cookie chứa thông tin username
+  // khi đăng nhập thành công
+};
